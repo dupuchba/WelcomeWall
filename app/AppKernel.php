2f550915db9f66d22f2433cb41173ee033e7ef5d
Baptiste DUPUCH <baptiste.dupuch@gmail.com>
2012-02-08 02:39:02 -0800

    Installation of VichGeographicalBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Kenavo\WelcomeWallBundle\KenavoWelcomeWallBundle(),
+            new Vich\GeographicalBundle\VichGeographicalBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
